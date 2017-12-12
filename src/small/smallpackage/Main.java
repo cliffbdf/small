256aa2608a2ab809a5033f0d2b1f193d728c03c6
Cliff Berg <cliff@cliffberg.com>
2017-12-12 17:28:53 -0500

    misc

--- src/small/smallpackage/Main.java
+++ src/small/smallpackage/Main.java
@@ -1,11 +1,7 @@
 package smallpackage;
 
-//import java.sql.*;
-
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Greetings!");
-		//Exception ex = new java.sql.SQLException("a SQL exception");
-		//System.out.println(ex.getMessage());
 	}
 }
